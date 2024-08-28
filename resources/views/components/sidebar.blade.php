ee8ed83673e3259cc471b78f4802b1178389cfe2
pimphand <faisaldwiki69@gmail.com>
2024-08-28 19:06:40 +0700

    first commit

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,20 @@
+<div class="srcollbar" data-simplebar>
+    <ul class="menu" data-fc-type="accordion">
+        <li class="menu-title">Menu</li>
+
+        <li class="menu-item">
+            <a href="index.html" class="menu-link">
+                <span class="menu-icon"><i class="mgc_home_3_line"></i></span>
+                <span class="menu-text"> Dashboard </span>
+            </a>
+        </li>
+
+        <li class="menu-title">Master</li>
+        <li class="menu-item">
+            <a href="index.html" class="menu-link">
+                <span class="menu-icon"><i class="mgc_home_3_line"></i></span>
+                <span class="menu-text"> Kategori </span>
+            </a>
+        </li>
+    </ul>
+</div>
